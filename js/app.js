2834a0368fda03c572eda25979849bdf94e97ac2
Henry Smith <h.f.smithz99@gmail.com>
2023-09-27 12:40:44 -0400

    added excercise 3

--- js/app.js
+++ js/app.js
@@ -21,3 +21,14 @@ Exercise 2:
 //push allows me to add elements to an array at the end of the array
 foods.push('pizza', 'cheeseburger');
 console.log('Exercise 2 Result:\n', foods);
+/*
+Exercise 3:
+  - Add the string 'taco' to the foods array so that 'taco' is the first food in the array.
+*/
+
+// Complete Exercise 3 below...
+
+//unshift allows me to add an element to the front of an array
+foods.unshift('taco');
+
+console.log('Exercise 3 Result:\n', foods);
